b0957dd71df086d545f7c70abf55d084aa92dac5
garbageman <dnagle@umd.edu>
2016-11-11 00:29:04 -0500

    Added jekyll

---
+++ StructuredLearningProject/src/org/structuredlearning/datastructures/graphs/WGraph.java
@@ -0,0 +1,34 @@
+package org.structuredlearning.datastructures.graphs;
+
+import java.util.Set;
+
+public class WGraph<E> {
+	
+	public WGraph() {
+		
+	}
+	
+	public boolean addVertex(E element) {
+		return false;
+	}
+	
+	public boolean addEdge(E firstElement, E secondElement, int edgeWeight) {
+		return false;
+	}
+	
+	public Set<E> getNeighbors(E element) {
+		return null;
+	}
+	
+	public Set<E> vertices() {
+		return null;
+	}
+	
+	public boolean contains(E element) {
+		return false;
+	}
+	
+	public int size() {
+		return 0;
+	}
+}
---
+++ StructuredLearningProject/src/org/structuredlearning/datastructures/linear/Stack.java
@@ -0,0 +1,58 @@
+package org.structuredlearning.datastructures.linear;
+
+/**
+ * This class is a generic stack
+ * @author damiennagle
+ *
+ * @param <E>
+ */
+public class Stack<E> {
+	
+	public Stack() {
+		
+	}
+	
+	/**
+	 * Returns if the stack is empty
+	 * @return true if stack is empty false otherwise
+	 */
+	public boolean empty() {
+		return false;
+	}
+	
+	/**
+	 * Returns the element on the top of the stack without removing it
+	 * @return Element on the top of the stack
+	 */
+	public E peek() {
+		return null;
+	}
+	
+	/**
+	 * Removes the element at the top of the stack
+	 * Returns the element removed from the stack
+	 * @return element removed from stack
+	 */
+	public E pop() {
+		return null;
+	}
+	
+	/**
+	 * Pushes an element on to the top of the stack
+	 * returns the element pushed on to the top of the stack
+	 * @param item
+	 * @return Element pushed onto stack
+	 */
+	public E push(E item) {
+		return null;
+	}
+	
+	/**
+	 * Returns the size of the stack
+	 * @return Size of stack
+	 */
+	public int size() {
+		return 0;
+	}
+
+}
